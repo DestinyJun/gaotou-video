27b54eb3d14e4884815a54b1f869dee0064f434a
DestinyJun <429771890@qq.com>
2018-09-09 14:07:57 +0800

    常规修改（第六次需求修改）： (1)还差服务区收入排名修改

--- src/app/business/service-data/service-data.component.ts
+++ src/app/business/service-data/service-data.component.ts
@@ -92,6 +92,7 @@ export class ServiceDataComponent implements OnInit {
   public eventAlertInfoUpTitle: string;
   // 服务区厕所监控
   public serversToiletAlertShow = false;
+  public waitTitle: string;
 
   /***********************右边************************/
   // 业态经营数据前十排名
@@ -891,12 +892,18 @@ export class ServiceDataComponent implements OnInit {
     this.eventListInfo = e;
   }
   public eventInfoUpClick(e): void {
-    this.eventAlertInfoUp = true;
-    this.eventAlertInfoUpTitle = e;
+    if (e !== '经营类') {
+      this.waitTitle = e;
+      this.serversToiletAlertShow = true;
+    } else {
+      this.eventAlertInfoUpTitle = e;
+      this.eventAlertInfoUp = true;
+    }
   }
   // 厕所弹窗
-  public openServersToiletAlert() {
+  public openServersToiletAlert(e) {
     document.body.className = 'ui-overflow-hidden';
+    this.waitTitle = e;
     this.serversToiletAlertShow = true;
   }
   public closeServersToiletAlert() {
@@ -1150,144 +1157,10 @@ export class ServiceDataComponent implements OnInit {
        mapStyle: {
          'styleJson': [
            {
-             "featureType": "water",
-             "elementType": "all",
-             "stylers": {
-               "color": "#021019"
-             }
-           },
-           {
-             "featureType": "highway",
-             "elementType": "geometry.fill",
-             "stylers": {
-               "color": "#000000"
-             }
-           },
-           {
-             "featureType": "highway",
-             "elementType": "geometry.stroke",
-             "stylers": {
-               "color": "#147a92"
-             }
-           },
-           {
-             "featureType": "arterial",
-             "elementType": "geometry.fill",
-             "stylers": {
-               "color": "#000000"
-             }
-           },
-           {
-             "featureType": "arterial",
-             "elementType": "geometry.stroke",
-             "stylers": {
-               "color": "#0b3d51"
-             }
-           },
-           {
-             "featureType": "local",
-             "elementType": "geometry",
-             "stylers": {
-               "color": "#000000"
-             }
-           },
-           {
-             "featureType": "land",
-             "elementType": "all",
-             "stylers": {
-               "color": "#08304b"
-             }
-           },
-           {
-             "featureType": "railway",
-             "elementType": "geometry.fill",
-             "stylers": {
-               "color": "#000000"
-             }
-           },
-           {
-             "featureType": "railway",
-             "elementType": "geometry.stroke",
-             "stylers": {
-               "color": "#08304b"
-             }
-           },
-           {
-             "featureType": "subway",
-             "elementType": "geometry",
-             "stylers": {
-               "lightness": -70
-             }
-           },
-           {
-             "featureType": "building",
-             "elementType": "geometry.fill",
-             "stylers": {
-               "color": "#000000"
-             }
-           },
-           {
-             "featureType": "all",
-             "elementType": "labels.text.fill",
-             "stylers": {
-               "color": "#857f7f"
-             }
-           },
-           {
-             "featureType": "all",
-             "elementType": "labels.text.stroke",
-             "stylers": {
-               "color": "#000000"
-             }
-           },
-           {
-             "featureType": "building",
-             "elementType": "geometry",
-             "stylers": {
-               "color": "#022338"
-             }
-           },
-           {
-             "featureType": "green",
-             "elementType": "geometry",
-             "stylers": {
-               "color": "#062032"
-             }
-           },
-           {
-             "featureType": "boundary",
-             "elementType": "all",
-             "stylers": {
-               "color": "#1e1c1c"
-             }
-           },
-           {
-             "featureType": "manmade",
-             "elementType": "geometry",
-             "stylers": {
-               "color": "#022338"
-             }
-           },
-           {
-             "featureType": "poi",
-             "elementType": "all",
-             "stylers": {
-               "visibility": "off"
-             }
-           },
-           {
-             "featureType": "all",
-             "elementType": "labels.icon",
-             "stylers": {
-               "visibility": "off"
-             }
-           },
-           {
-             "featureType": "all",
-             "elementType": "labels.text.fill",
-             "stylers": {
-               "color": "#2da0c6",
-               "visibility": "on"
+             'featureType': 'background',
+             'elementType': 'all',
+             'stylers': {
+               'color': '#2da0c6ff'
              }
            }
          ]
