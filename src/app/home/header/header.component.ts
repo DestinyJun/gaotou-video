4518c925b2e3b15d9452ac3dea6a638442d0680e
DestinyJun <429771890@qq.com>
2018-10-29 10:54:23 +0800

    常规修改（第二十次需求修改）：  （1）增加省级地图分布  （2）修改服务区事件排列方式

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -23,6 +23,7 @@ export class HeaderComponent implements OnInit {
   public serviceZonePersonAlert = false;
   public cityPersonAlert = false;
   public cityOptions = {};
+  public cityParentOptions = {};
   constructor(
     private tools: ToolsService,
     private routerInfo: ActivatedRoute,
@@ -85,8 +86,12 @@ export class HeaderComponent implements OnInit {
       this.serviceZonePersonAlert = false;
       this.cityPersonAlert = false;
   }
-  // 省市级地图函数
+  // 省市级全国地图分布函数
   public buildEchart() {
+    // 配置
+    const max = 480, min = 9;
+    const maxSize4Pin = 100, minSize4Pin = 20;
+    // 全国
     const mapName = 'china';
     const data = [
       {name: '北京', value: 177},
@@ -123,15 +128,15 @@ export class HeaderComponent implements OnInit {
     ];
     const geoCoordMap = {};
     const toolTipData = [
-      {name: '北京', value: [{name: '文科', value: 95}, {name: '理科', value: 82}]},
-      {name: '天津', value: [{name: '文科', value: 22}, {name: '理科', value: 20}]},
-      {name: '河北', value: [{name: '文科', value: 60}, {name: '理科', value: 42}]},
-      {name: '山西', value: [{name: '文科', value: 40}, {name: '理科', value: 41}]},
-      {name: '内蒙古', value: [{name: '文科', value: 23}, {name: '理科', value: 24}]},
-      {name: '辽宁', value: [{name: '文科', value: 39}, {name: '理科', value: 28}]},
-      {name: '吉林', value: [{name: '文科', value: 41}, {name: '理科', value: 41}]},
+      {name: '贵阳', value: [{name: '文科', value: 95}, {name: '理科', value: 82}]},
+      {name: '六盘水', value: [{name: '文科', value: 22}, {name: '理科', value: 20}]},
+      {name: '遵义', value: [{name: '文科', value: 60}, {name: '理科', value: 42}]},
+      {name: '铜人', value: [{name: '文科', value: 40}, {name: '理科', value: 41}]},
+      {name: '毕节', value: [{name: '文科', value: 23}, {name: '理科', value: 24}]},
+      {name: '黔西南', value: [{name: '文科', value: 39}, {name: '理科', value: 28}]},
+      {name: '黔东南', value: [{name: '文科', value: 41}, {name: '理科', value: 41}]},
       {name: '黑龙江', value: [{name: '文科', value: 35}, {name: '理科', value: 31}]},
-      {name: '上海', value: [{name: '文科', value: 12}, {name: '理科', value: 12}]},
+      {name: '兴义', value: [{name: '文科', value: 12}, {name: '理科', value: 12}]},
       {name: '江苏', value: [{name: '文科', value: 47}, {name: '理科', value: 45}]},
       {name: '浙江', value: [{name: '文科', value: 57}, {name: '理科', value: 57}]},
       {name: '安徽', value: [{name: '文科', value: 57}, {name: '理科', value: 52}]},
@@ -162,8 +167,6 @@ export class HeaderComponent implements OnInit {
       // 地区经纬度
       geoCoordMap[name] = v.properties.cp;
     });
-    const max = 480, min = 9;
-    const maxSize4Pin = 100, minSize4Pin = 20;
     const convertData = function (datas) {
       const res = [];
       for (let i = 0; i < datas.length; i++) {
@@ -177,6 +180,53 @@ export class HeaderComponent implements OnInit {
       }
       return res;
     };
+    // 贵州
+    const mapParentName = '贵州';
+    const parentData = [
+      {name: '贵阳市', value: 177},
+      {name: '六盘水市', value: 42},
+      {name: '遵义市', value: 102},
+      {name: '安顺市', value: 102},
+      {name: '毕节市', value: 47},
+      {name: '铜仁市', value: 81},
+      {name: '黔西南布依族苗族自治州', value: 67},
+      {name: '黔东南苗族侗族自治州', value: 82},
+      {name: '黔南布依族苗族自治州', value: 66},
+    ];
+    const geoCoordParentMap = {};
+    const toolTipParentData = [
+      {name: '贵阳市', value: [{name: '文科', value: 95}, {name: '理科', value: 82}]},
+      {name: '六盘水市', value: [{name: '文科', value: 22}, {name: '理科', value: 20}]},
+      {name: '遵义市', value: [{name: '文科', value: 60}, {name: '理科', value: 42}]},
+      {name: '安顺市', value: [{name: '文科', value: 40}, {name: '理科', value: 41}]},
+      {name: '毕节市', value: [{name: '文科', value: 23}, {name: '理科', value: 24}]},
+      {name: '铜仁市', value: [{name: '文科', value: 39}, {name: '理科', value: 28}]},
+      {name: '黔西南布依族苗族自治州', value: [{name: '文科', value: 41}, {name: '理科', value: 41}]},
+      {name: '黔东南苗族侗族自治州', value: [{name: '文科', value: 35}, {name: '理科', value: 31}]},
+      {name: '黔南布依族苗族自治州', value: [{name: '文科', value: 12}, {name: '理科', value: 12}]},
+    ];
+    const mapParentFeatures = this.es.getMap(mapParentName).geoJson.features;
+    mapParentFeatures.forEach(function(v) {
+      // 地区名称
+      const name = v.properties.name;
+      // 地区经纬度
+      geoCoordParentMap[name] = v.properties.cp;
+    });
+    const convertParentData = function (datas) {
+      console.log(datas);
+      const res = [];
+      for (let i = 0; i < datas.length; i++) {
+        const geoCoord = geoCoordParentMap[datas[i].name];
+        if (geoCoord) {
+          res.push({
+            name: datas[i].name,
+            value: geoCoord.concat(datas[i].value),
+          });
+        }
+      }
+      return res;
+    };
+    // 数据对象
     this.cityOptions = {
       tooltip: {
         trigger: 'item',
@@ -338,6 +388,166 @@ export class HeaderComponent implements OnInit {
           },
           zlevel: 6,
           data: convertData(data),
+        },
+      ]
+    };
+    this.cityParentOptions = {
+      tooltip: {
+        trigger: 'item',
+        formatter: function (params) {
+          if (typeof(params.value)[2] === 'undefined') {
+            let toolTiphtml = '';
+            for (let i = 0; i < toolTipParentData.length; i++) {
+              if (params.name === toolTipParentData[i].name) {
+                toolTiphtml += toolTipParentData[i].name + ':<br>';
+                for (let j = 0; j < toolTipParentData[i].value.length; j++) {
+                  toolTiphtml += toolTipParentData[i].value[j].name + ':' + toolTipParentData[i].value[j].value + '<br>';
+                }
+              }
+            }
+            return toolTiphtml;
+          } else {
+            let toolTiphtml = '';
+            for (let i = 0; i < toolTipParentData.length; i++) {
+              if (params.name === toolTipParentData[i].name) {
+                toolTiphtml += toolTipParentData[i].name + ':<br>';
+                for (let j = 0; j < toolTipParentData[i].value.length; j++) {
+                  toolTiphtml += toolTipParentData[i].value[j].name + ':' + toolTipParentData[i].value[j].value + '<br>';
+                }
+              }
+            }
+            return toolTiphtml;
+          }
+        }
+      },
+      visualMap: {
+        show: true,
+        min: 0,
+        max: 200,
+        left: 'left',
+        top: 'bottom',
+        text: ['高', '低'], // 文本，默认为数值文本
+        calculable: true,
+        seriesIndex: [1],
+        inRange: {
+          // color: ['#3B5077', '#031525'] // 蓝黑
+          // color: ['#ffc0cb', '#800080'] // 红紫
+          // color: ['#3C3B3F', '#605C3C'] // 黑绿
+          // color: ['#0f0c29', '#302b63', '#24243e'] // 黑紫黑
+          // color: ['#23074d', '#cc5333'] // 紫红
+          color: ['#00467F', '#A5CC82'] // 蓝绿
+          // color: ['#1488CC', '#2B32B2'] // 浅蓝
+          // color: ['#00467F', '#A5CC82'] // 蓝绿
+          // color: ['#00467F', '#A5CC82'] // 蓝绿
+          // color: ['#00467F', '#A5CC82'] // 蓝绿
+          // color: ['#00467F', '#A5CC82'] // 蓝绿
+
+        }
+      },
+      geo: {
+        show: true,
+        map: mapParentName,
+        label: {
+          normal: {
+            show: false
+          },
+          emphasis: {
+            show: false,
+          }
+        },
+        roam: true,
+        itemStyle: {
+          normal: {
+            areaColor: '#031525',
+            borderColor: '#3B5077',
+          },
+          emphasis: {
+            areaColor: '#2B91B7',
+          }
+        }
+      },
+      series: [
+        {
+          name: '散点',
+          type: 'scatter',
+          coordinateSystem: 'geo',
+          data: convertParentData(parentData),
+          symbolSize: function (val) {
+            return val[2] / 10;
+          },
+          label: {
+            normal: {
+              formatter: '{b}',
+              position: 'right',
+              show: true
+            },
+            emphasis: {
+              show: true
+            }
+          },
+          itemStyle: {
+            normal: {
+              color: '#05C3F9'
+            }
+          }
+        },
+        {
+          type: 'map',
+          map: mapParentName,
+          geoIndex: 0,
+          aspectScale: 0.75, // 长宽比
+          showLegendSymbol: false, // 存在legend时显示
+          label: {
+            normal: {
+              show: true
+            },
+            emphasis: {
+              show: false,
+              textStyle: {
+                color: '#fff'
+              }
+            }
+          },
+          roam: true,
+          itemStyle: {
+            normal: {
+              areaColor: '#031525',
+              borderColor: '#3B5077',
+            },
+            emphasis: {
+              areaColor: '#2B91B7'
+            }
+          },
+          animation: false,
+          data: parentData
+        },
+        {
+          name: '点',
+          type: 'scatter',
+          coordinateSystem: 'geo',
+          symbol: 'pin', // 气 泡
+          symbolSize: function (val) {
+            const a = (maxSize4Pin - minSize4Pin) / (max - min);
+            // const b = minSize4Pin - a * min;
+            const b = maxSize4Pin - a * max;
+            return a * val[2] + b;
+          },
+          label: {
+            normal: {
+              show: true,
+              textStyle: {
+                color: '#fff',
+                fontSize: 9,
+              }
+            }
+          },
+          itemStyle: {
+            normal: {
+              color: '#F62157', // 标志颜色
+            }
+          },
+          zlevel: 6,
+          data: convertParentData(parentData),
         },
       ]
     };
